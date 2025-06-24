266818e6414fc8602595516f2a94ff8d71f7a9fa
CarlosIMAlberto <carlosalberto.amic@gmail.com>
2025-06-24 22:40:33 +0100

    Add o ficheiro README do Verificador de Maioridade
    Adicionado README com instruções de utilização e descrição do Verificador de Maioridade em C#

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Pojecto 4 rumo a maestria em c#");
+            string nome;
+            byte idade;
+            Console.WriteLine("Digite seu nome");
+            nome = Console.ReadLine();
+            Console.WriteLine("Digite sua Idade");
+            idade = byte.Parse(Console.ReadLine());
+            if (idade >= 18)
+            {
+                Console.WriteLine("O senhor(a) tem {idade} e maior de idade " + idade);
+
+            }
+            else {
+                Console.WriteLine(" O senhor(a) tem {idade} e menor de idade " + idade);
+            }
+
+        }
+    }
+}
